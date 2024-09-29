06aa2b64cc03aed69cb6877e343da2e7c7289d04
AmineFRAJ <amine0924@gmail.com>
2024-09-29 23:45:39 +0100

    auth/user back

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, (error) => {
-    error ? console.log(error)
+  error
+    ? console.log(error)
     : console.log(`⚡⚡⚡ Server is running on http://127.0.0.1:${PORT}`);
-})
+});
 
-const connectDB = require('./config/connectDB')
+const connectDB = require("./config/connectDB");
 
-connectDB()
+connectDB();
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+// Product routes
+app.use("/api/products", require("./routes/productRoutes"));
 
-app.use('/api/products', require('./routes/productRoutes'))
+//auth routes
+app.use("/api/auth", require("./routes/authRoutes"));
+
+// User routes
+app.use("/api/users", require("./routes/userRoutes"));
