dfe534f1500cb4f3225e773ecfcb19005394e950
AmineFRAJ <amine0924@gmail.com>
2024-10-14 13:00:52 +0100

    cors config7

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://gadget-hub-client-git-master-aminefrajs-projects.vercel.app/'); // Allow frontend origin
+  res.header('Access-Control-Allow-Origin', 'https://gadget-hub-client-63oq20pek-aminefrajs-projects.vercel.app/'); // Allow frontend origin
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Credentials', 'true'); // Allow cookies if needed
