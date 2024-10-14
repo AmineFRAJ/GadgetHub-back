00f87cce5fe290070f7bfc77747ed555dfb293df
AmineFRAJ <amine0924@gmail.com>
2024-10-14 12:27:22 +0100

    cors config1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
-const allowedOrigins = ['https://gadget-hub-client-pfxhivgrc-aminefrajs-projects.vercel.app'];
+const allowedOrigins = ['https://gadget-hub-client-git-master-aminefrajs-projects.vercel.app/'];
 
 app.use(cors({
   origin: function (origin, callback) {
